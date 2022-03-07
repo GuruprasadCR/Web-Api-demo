5da94a8c7c88b3702189999cc31674c1f7933226
charanhu1 <charanhumail@gmail.com>
2022-03-07 18:17:47 +0530

    Revert

--- App_Start/WebApiConfig.cs
+++ App_Start/WebApiConfig.cs
@@ -10,7 +10,7 @@ namespace WebApi
     {
         public static void Register(HttpConfiguration config)
         {
-            // Web API configuration and servicess
+            // Web API configuration and services
 
             // Web API routes
             config.MapHttpAttributeRoutes();
